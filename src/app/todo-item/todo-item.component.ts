1384ef794d0463a74f14ec27f97d668954a0dca7
Your Name <you@example.com>
2023-06-26 23:35:34 -0300

    feat: inicio do desenvolvimento do drag drop

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent {
+  @Input()
+  id:number;
 
+  onDrop(event:any){
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+  }
+  onDragOver(event:any) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+  drag(ev:any) {
+    console.log(ev.target.id);
+
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
 }
