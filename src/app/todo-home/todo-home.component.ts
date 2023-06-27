9bbc28ae3803e899f0ece7c38904ed69788b5784
Your Name <you@example.com>
2023-06-26 23:16:53 -0300

    feat: preparando html para drag drop

--- src/app/todo-home/todo-home.component.ts
+++ src/app/todo-home/todo-home.component.ts
@@ -1,80 +1,37 @@
   import { Component, OnInit } from '@angular/core';
   import { AsyncSubject, forkJoin, from, Observable, of } from 'rxjs';
 
-  @Component({
-    selector: 'app-todo-home',
-    templateUrl: './todo-home.component.html',
-    styleUrls: ['./todo-home.component.scss']
-  })
-  export class TodoHomeComponent implements OnInit  {
-    count:number =10;
+@Component({
+  selector: 'app-todo-home',
+  templateUrl: './todo-home.component.html',
+  styleUrls: ['./todo-home.component.scss']
+})
+export class TodoHomeComponent implements OnInit  {
+  count:number =10;
+  accumulatorPxCounter = 2.5;
+  maxAcumulatorLR = 5;
+  maxLengthForLR = 2;
+  headers = [' ToDos',' Active',' Completed'];
+  ngOnInit() {
 
-
-   ngOnInit() {
-
-     this.adicionarInvestigado();
   }
 
 
-
-
-
-  async adicionarInvestigado(){
-    let behavior:AsyncSubject<any> = new AsyncSubject();
-    let enderecoSubject:AsyncSubject<any> = new AsyncSubject<any>();
-
-    let lista:Array<AsyncSubject<any>> = []
-    lista.push(behavior);
-    lista.push(enderecoSubject);
-
-    console.log('adicionarinvestigado')
-    this.qualificacao(behavior,true);
-    this.adicionarEndereco(enderecoSubject);
-
-    forkJoin(lista).subscribe(_=>this.salvar());
-  }
-  adicionarEndereco(enderecoSubject:AsyncSubject<any>){
-    setTimeout(()=>{
-      let listaObservable:Array<Observable<number>> = [];
-      let primeiro = of(1);
-      let segundo = of(2);
-      listaObservable.push(primeiro,segundo);
-      forkJoin(listaObservable).subscribe(res=>{
-        console.log(res[0]);
-        console.log(res[1]);
-
-        enderecoSubject.next(true);
-        enderecoSubject.complete();
-      })
-    },6000)
-  }
-
-  qualificacao(asyncObject:AsyncSubject<any>,hasMunicipio:boolean){
-    console.log('qualificacao');
-    if(hasMunicipio){
-      setTimeout(() => {
-        console.log('nacionalidade');
-        asyncObject.next(true);
-        asyncObject.complete();
-
-      }, 3000);
-    }else{
-      asyncObject.next(true);
-      asyncObject.complete();
+  calcularDiametroCounter(count:number){
+    if(count){
+      return this.handleAcumulator(count.toString().length)
     }
-    console.log('uf')
+    return {};
   }
 
-  // const click$ = fromEvent(document, 'click').pipe(
-  //   map((e: MouseEvent) => ({
-  //     x: e.clientX,
-  //     y: e.clientY,
-  //     id: Math.random()
-  //   })),
-  //   tap(addHtmlElement),
-  //   mergeMap(coords => subject.pipe(tap(v => setElementText(coords.id, v))))
-  // );
-  salvar(){
-    console.log('salvar');
+  handleAcumulator(length:number){
+    if(length > this.maxLengthForLR){
+      return {
+        padding:length * this.accumulatorPxCounter + 'px ' + ' ' + this.maxAcumulatorLR + 'px'
+      }
+    }
+    return {
+      padding:length * this.accumulatorPxCounter + 'px'
+    }
   }
-  }
+}
